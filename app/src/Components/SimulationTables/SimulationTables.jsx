06ede6c04229b6c4493665bb32fa8bc92d40a8f9
Georgey1127 <jorgebolivarm27@gmail.com>
2024-11-19 20:59:27 -0500

    feat: add MIPS application structure and styling, update DropArea and SimulationTables components

--- app/src/Components/SimulationTables/SimulationTables.jsx
+++ app/src/Components/SimulationTables/SimulationTables.jsx
@@ -2,33 +2,33 @@ import React from 'react';
 import './SimulationTables.css';
 const SimulationTables = ({ registers, memory }) => {
   return (
-    <div id="simulation-tables">
-      <table id="registerTable">
+    <div id="simulation-tables" className='tables-container'>
+      <table id="registerTable" className='table'>
         <thead>
-          <tr>
+          <tr className='values'>
             <th>Register</th>
             <th>Value</th>
           </tr>
         </thead>
         <tbody>
           {Object.keys(registers).map((reg) => (
-            <tr key={reg}>
+            <tr key={reg} className='values'>
               <td>{reg}</td>
               <td>{`0x${registers[reg].toString(16).toUpperCase()}`}</td>
             </tr>
           ))}
         </tbody>
       </table>
-      <table id="ramTable">
+      <table id="ramTable" className='table' >
         <thead>
-          <tr>
+          <tr className='values'>
             <th>Address</th>
             <th>Value</th>
           </tr>
         </thead>
         <tbody>
           {Object.keys(memory).map((addr) => (
-            <tr key={addr}>
+            <tr key={addr} className='values'>
               <td>{`0x${parseInt(addr).toString(16).toUpperCase()}`}</td>
               <td>{`0x${memory[addr].toString(16).toUpperCase()}`}</td>
             </tr>
