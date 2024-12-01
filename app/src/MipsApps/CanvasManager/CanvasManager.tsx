f9a8daed0ea5271eef1c5e72bad1e159674cb6e2
CristhianAc <cristhian_agamez@outlook.com>
2024-12-01 01:44:37 -0500

    feat: implement CanvasContext and CanvasManager for enhanced canvas handling

--- app/src/MipsApps/CanvasManager/CanvasManager.tsx
+++ app/src/MipsApps/CanvasManager/CanvasManager.tsx
@@ -1,5 +1,18 @@
-import React from 'react'
-const CanvasManager = () => {   
+import React, { useEffect } from "react";
 
-}
-export default CanvasManager
+import "./CanvasManager.css";
+import { useCanvasContext } from "../CanvasContext/CanvasContext";
+const CanvasManager = () => {
+  const { canvasRef, createElementWithSvg } = useCanvasContext();
+
+  useEffect(() => {
+    const svgPath = "/components/Control.svg";
+    console.log(`Loading SVG from: ${svgPath}`);
+
+    createElementWithSvg(svgPath, { x: 24 ,y: 24, xS: 2.4, yS: 2.1 });
+  }, [createElementWithSvg]);
+
+  return <canvas ref={canvasRef} id="canvas" width="800" height="600"></canvas>;
+};
+
+export default CanvasManager;
