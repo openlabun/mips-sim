dd1a1d66041705364fca554437ac97bf9959a59d
CristhianAc <cristhian_agamez@outlook.com>
2024-12-01 14:37:00 -0500

    feat: update CanvasManager and CanvasArrow components for improved positioning and arrow drawing logic

--- app/src/MipsApps/CanvasManager/CanvasManager.tsx
+++ app/src/MipsApps/CanvasManager/CanvasManager.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 import "./CanvasManager.css";
 import { useCanvasContext } from "../CanvasContext/CanvasContext";
-import { CanvasArrow, Control, Memory, Register, InstructionRegister } from "./Components";
+import {
+  CanvasArrow,
+  Control,
+  Memory,
+  Register,
+  InstructionRegister,
+} from "./Components";
 import { Ids } from "../../model/Ids.enum";
 
 const CanvasManager = ({ value }: { value: string }) => {
@@ -10,10 +16,10 @@ const CanvasManager = ({ value }: { value: string }) => {
   return (
     <div className="canvas-container">
       <canvas ref={canvasRef} id="canvas" width="800" height="600"></canvas>
-      <Control x={650} y={0} xS={3} yS={2} value={value} />
-      <Memory x={100} y={350} xS={3} yS={2} value={value} />
-      <Register x={800} y={350} xS={3} yS={2} value={value} />
-      <InstructionRegister x={430} y={350} xS={3} yS={2} value={value} />
+      <Control x={650} y={0} xS={1} yS={1} value={value} />
+      <Memory x={2} y={350} xS={1.2} yS={1.2} value={value} />
+      <Register x={800} y={350} xS={1.2} yS={1.2} value={value} />
+      <InstructionRegister x={430} y={350} xS={1.2} yS={1.2} value={value} />
       <CanvasArrow fromId={Ids.MEMORY} toId={Ids.CONTROL} />
     </div>
   );
